7ce197e7933c7691cb5b43de305177f85f1b91e9
Mr-Nancy <mister.nansi@gmail.com>
2017-08-20 23:58:19 +0300

    .

--- alarm_extension.js
+++ alarm_extension.js
@@ -34,7 +34,7 @@ new (function() {
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-	    [' ', 'my first block', 'my_first_block'],
+	    [' ', 'my first block', 'my_first-block'],
             ['', 'run alarm after %n seconds', 'set_alarm', '2'],
             ['h', 'when alarm goes off', 'when_alarm'],
         ]
