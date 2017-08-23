250c687a684a917322da91fbdada42973429de28
Mr-Nancy <mister.nansi@gmail.com>
2017-08-23 17:29:44 +0300

    ю

--- alarm_extension.js
+++ alarm_extension.js
@@ -38,7 +38,7 @@ new (function() {
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-	        ['R', 'my first block', 'getTemperature'],
+	        ['R', 'Temperature', 'getTemperature'],
             ['', 'run alarm after %n seconds', 'set_alarm', '2'],
             ['h', 'when alarm goes off', 'when_alarm'],
         ]
